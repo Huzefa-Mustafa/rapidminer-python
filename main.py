de08f2ca0a82dda0b649a3395af9bca26723ee2d
Huzefa-Mustafa <huzefahdk@gmail.com>
2022-06-27 18:01:46 +0200

    updated requirements, added textscripts

--- main.py
+++ main.py
@@ -2,15 +2,7 @@ import rapidminer
 import os
 rm_home="C:\Program Files\RapidMiner\RapidMiner Studio"
 # If you don't want to see the log messages of the operations, use rm_stdout=open(os.devnull,"w")
-connector = rapidminer.Studio(rm_home, rm_stdout=None)
+connector = rapidminer.Studio(rm_home, rm_stdout=open(os.devnull,"w"))
 df = connector.read_resource("//Samples/data/Iris")
 print("The result is a pandas DataFrame:")
 print(df.head())
-# project = rapidminer.Project("RapidMiner_Python")
-
-# df = project.read("data/mydata")
-# df = rapidminer.Project().read("local/file/path.rmhdf5table")
-
-# project.write(df, "data/mydata_modified")
-
-#con
