b0ff949d34a8999a57d0cd8580a31d44a1f1a3ed
Huzefa-Mustafa <huzefahdk@gmail.com>
2022-06-28 01:10:39 +0200

    updated main.py

--- scripts/main.py
+++ scripts/main.py
@@ -5,4 +5,9 @@ from utils.connection import connector
 def rm_main():
     df = connector.read_resource("//Samples/data/Iris")
     print("The result is a pandas DataFrame:")
+    print(df.head())
     return df.head()
+
+
+if __name__ == '__main__':
+	rm_main()
